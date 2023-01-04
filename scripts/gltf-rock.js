65bd6ba9dc8f07e6868eeb315edc0ea1f2cb026c
Knar <knarxr@protonmail.com>
2023-01-04 10:09:36 -0600

    reworked function

--- scripts/gltf-rock.js
+++ scripts/gltf-rock.js
@@ -1,111 +1,54 @@
 function generateRock(index) {
     //generate vertices of icosahedron using 3 golden ratio rectangles
+    const g = (1 + Math.sqrt(5)) / 2; //golden ratio
+    var vertices = [
+        [g, 1, 0], [g, -1, 0], [-g, -1, 0], [-g, 1, 0],
+        [1, 0, g], [-1, 0, g], [-1, 0, -g], [1, 0, -g],
+        [0, g, 1], [0, g, -1], [0, -g, -1], [0, -g, 1]
+    ]
+
     //hard-code triangle array using vertices (or find an elegant way..)
+    var triangles = [
+        [vertices[0], vertices[4], vertices[8]],
+        [vertices[0], vertices[1], vertices[4]],
+        [vertices[0], vertices[1], vertices[7]],
+        [vertices[0], vertices[7], vertices[9]],
+        [vertices[0], vertices[8], vertices[9]],
+        [vertices[2], vertices[5], vertices[11]],
+        [vertices[2], vertices[11], vertices[10]],
+        [vertices[2], vertices[10], vertices[6]],
+        [vertices[2], vertices[6], vertices[3]],
+        [vertices[2], vertices[3], vertices[5]],
+        [vertices[10], vertices[1], vertices[11]],
+        [vertices[10], vertices[7], vertices[1]],
+        [vertices[11], vertices[1], vertices[4]],
+        [vertices[11], vertices[4], vertices[5]],
+        [vertices[3], vertices[9], vertices[6]],
+        [vertices[4], vertices[5], vertices[8]],
+        [vertices[5], vertices[8], vertices[3]],
+        [vertices[6], vertices[9], vertices[7]],
+        [vertices[8], vertices[3], vertices[9]],
+        [vertices[7], vertices[6], vertices[10]]
+    ]
+
     //sub-divide triangles and update vertex/triangle array
+    
     //for each vertex, offset by 3d perlin noise value
+
     //generate mesh
 
-    const geometry = new THREE.BufferGeometry();
+    var geometryVertices = [];
+    for (let triangle = 0; triangle < triangles.length; triangle++) {
+        for (let vertex = 0; vertex < 3; vertex++) {
+            for (let dimension = 0; dimension < 3; dimension++) {
+                geometryVertices.push(triangles[triangle][vertex][dimension])
+            }
+        }
+    }
 
-    const g = (1 + Math.sqrt(5)) / 2; //golden ratio
-    
-    let v1 = [g, 1, 0];
-    let v2 = [g, -1, 0];
-    let v3 = [-g, -1, 0];
-    let v4 = [-g, 1, 0];
-    let v5 = [1, 0, g];
-    let v6 = [-1, 0, g];
-    let v7 = [-1, 0, -g];
-    let v8 = [1, 0, -g];
-    let v9 = [0, g, 1];
-    let v10 = [0, g, -1];
-    let v11 = [0, -g, -1];
-    let v12 = [0, -g, 1];
-    
-    const vertices = new Float32Array( [
-        v1[0], v1[1], v1[2],
-        v5[0], v5[1], v5[2],
-        v9[0], v9[1], v9[2],
-    
-        v1[0], v1[1], v1[2],
-        v2[0], v2[1], v2[2],
-        v5[0], v5[1], v5[2],
-            
-        v1[0], v1[1], v1[2],
-        v2[0], v2[1], v2[2],
-        v8[0], v8[1], v8[2],
-            
-        v1[0], v1[1], v1[2],
-        v8[0], v8[1], v8[2],
-        v10[0], v10[1], v10[2],
-            
-        v1[0], v1[1], v1[2],
-        v9[0], v9[1], v9[2],
-        v10[0], v10[1], v10[2],
-        
-        v3[0], v3[1], v3[2],
-        v6[0], v6[1], v6[2],
-        v12[0], v12[1], v12[2],
-            
-        v3[0], v3[1], v3[2],
-        v12[0], v12[1], v12[2],
-        v11[0], v11[1], v11[2],
-            
-        v3[0], v3[1], v3[2],
-        v11[0], v11[1], v11[2],
-        v7[0], v7[1], v7[2],
-            
-        v3[0], v3[1], v3[2],
-        v7[0], v7[1], v7[2],
-        v4[0], v4[1], v4[2],
-            
-        v3[0], v3[1], v3[2],
-        v4[0], v4[1], v4[2],
-        v6[0], v6[1], v6[2],
-            
-    
-        v11[0], v11[1], v11[2],
-        v2[0], v2[1], v2[2],
-        v12[0], v12[1], v12[2],
-            
-        v12[0], v12[1], v12[2],
-        v2[0], v2[1], v2[2],
-        v5[0], v5[1], v5[2],
-            
-        v12[0], v12[1], v12[2],
-        v5[0], v5[1], v5[2],
-        v6[0], v6[1], v6[2],
-            
-        v5[0], v5[1], v5[2],
-        v6[0], v6[1], v6[2],
-        v9[0], v9[1], v9[2],
-            
-        v6[0], v6[1], v6[2],
-        v9[0], v9[1], v9[2],
-        v4[0], v4[1], v4[2],
-            
-        v9[0], v9[1], v9[2],
-        v4[0], v4[1], v4[2],
-        v10[0], v10[1], v10[2],
-            
-        v4[0], v4[1], v4[2],
-        v10[0], v10[1], v10[2],
-        v7[0], v7[1], v7[2],
-            
-        v7[0], v7[1], v7[2],
-        v10[0], v10[1], v10[2],
-        v8[0], v8[1], v8[2],
-            
-        v8[0], v8[1], v8[2],
-        v7[0], v7[1], v7[2],
-        v11[0], v11[1], v11[2],
-            
-        v11[0], v11[1], v11[2],
-        v8[0], v8[1], v8[2],
-        v2[0], v2[1], v2[2],
-    ] );
-    
-    geometry.setAttribute( 'position', new THREE.BufferAttribute( vertices, 3 ) );
+    const geometry = new THREE.BufferGeometry();
+    var geometryBuffer = new Float32Array(geometryVertices);
+    geometry.setAttribute( 'position', new THREE.BufferAttribute( geometryBuffer, 3 ) );
     
     const material = new THREE.MeshBasicMaterial( { color: 0x111111,
         fog: true } );
